7295589e8035f53b6be46daa2803398f72a5a2bd
LovelyLang <lovelymoomal001@protonmail.com>
2024-05-23 15:18:13 +0900

    no message

--- pdf/go_pdf_split.py
+++ pdf/go_pdf_split.py
@@ -3,4 +3,4 @@ if __name__ == '__main__':
     print('hi')
     dir = '/Users/user/Documents/paper_parser/pdf/pdf_data/Computational Finance'
     returns = pdf_splitter(dir,40, 0)
-    print(len(returns))
+    print(returns)
